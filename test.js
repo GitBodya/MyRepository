2c97b548ecb5900f82ac8e414b9b4338e6e6b4b4
Bohdan <bodyaluweb@gmail.com>
2021-06-05 19:30:10 +0300

    first commit

---
+++ test.js
@@ -0,0 +1,32 @@
+function f(){
+    //console.log(4/0);
+    //alert(9);
+   // console.log(9);
+  //  document.write('cv');
+   // console.log('sdd'*56);
+}
+
+//alert(88);
+let sd = +prompt('are you here',"Yes");
+
+//  console.log(sd);
+//  console.log(typeof(sd));
+
+let arra = ['1.gif','2.jpg', '3.png']
+           
+// console.log(arra[1]);
+// console.log(typeof(arra));
+// console.log(5 + + " dingo  - dog");
+
+let incr = 10,
+     decr = 10;
+
+     incr;
+     decr--;
+
+     console.log(++incr+" "+--decr);
+
+
+     'use strict'
+     a = 8;
+     console.log(a);
